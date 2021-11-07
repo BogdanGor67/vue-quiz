628e809643a7e209086a898730166d14c743f3d4
BogdanGor67 <gorbg67@yandex.ru>
2021-11-08 02:11:10 +0300

    commit

---
+++ src/components/E404.vue
@@ -0,0 +1,16 @@
+<template>
+	<div class="error-page">
+		Страница не найдена
+	</div>
+</template>
+
+<style>
+	.error-page {
+		width: 100vw;
+		height: 100vh;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		font-size: 36px;
+	}
+</style>
